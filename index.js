2c3602ab560807a451d7364312405782d6dbd07a
PLUTo <nawin3399#gmail.com>
2023-02-08 10:25:04 +0700

    create crud file

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.post("/test", (req, res) => {
+  res.send("asd")
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
