7ca4e9e450533d7fa8eea929b7fab726dae45c04
PLUTo <nawin3399#gmail.com>
2023-03-08 10:31:51 +0700

    update

--- src/MongoDBCRUD.js
+++ src/MongoDBCRUD.js
@@ -1,28 +1,31 @@
-
-// Description: REST API with MongoDB
-// npm install express mongoose body-parser // Run this file with node MongoDBREST.js
+// Description : REST API With MongoDB
+// npm install express mongoose body-parser
+// Run this file with node MongoDBREST.js
 // Test with Postman
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
 // Database connection
 mongoose.connect(
-    "mongodb://admin:SBFsqa14913@node40731-noderest.proen.app.ruk-com.cloud:11344",
+    "mongodb://admin://",
     {
-        useNewUrlParser: true,
+        userNewUrlParser: true,
         useUnifiedTopology: true,
     }
 );
+
 const Book = mongoose.model("Book", {
     id: Number,
     title: String,
     author: String,
 });
 
+const app = express();
+app.use(bodyParser.json());
 
-
-const app = express(); app.use(bodyParser.json());
-// Create
+//Create
 app.post("/books", async (req, res) => {
     try {
         const book = new Book(req.body);
@@ -33,19 +36,19 @@ app.post("/books", async (req, res) => {
         res.status(500).send(error);
     }
 });
-// Read all
+
+//Read all
 app.get("/books", async (req, res) => {
     try {
-        const books = await Book.find(); res.send(books);
+        const books = await Book.find();
+        res.send(books);
     } catch (error) {
         res.status(500).send(error);
     }
 });
 
-
-
-// Read one
-app.get("/books/:id", async (req, res) => {
+//Read one
+app.get("/books:id", async (req, res) => {
     try {
         const book = await Book.findOne(req.params.id);
         res.send(book);
@@ -53,8 +56,10 @@ app.get("/books/:id", async (req, res) => {
         res.status(500).send(error);
     }
 });
-// Update
-app.put("/books/:id", async (req, res) => {
+
+
+//Update
+app.put("/books:id", async (req, res) => {
     try {
         const book = await Book.findOneAndUpdate(req.params.id, req.body, {
             new: true,
@@ -66,18 +71,19 @@ app.put("/books/:id", async (req, res) => {
 });
 
 
-
 // Delete
-app.delete("/books/:id", async (req, res) => {
+app.delete("/books:id", async (req, res) => {
     try {
         const book = await Book.findOneAndDelete(req.params.id);
         res.send(book);
     } catch (error) {
+        res.status(500).send(error);
     }
-    res.status(500).send(error);
 });
+
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
 });
-console.log(`Server started at http://localhost:${PORT}`);
